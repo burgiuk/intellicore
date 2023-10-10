8d597a18bce3ede9110b4acf135ad9803e91d7e5
John Burgess <j_burgess12@yahoo.com>
2023-10-10 17:41:53 +0100

    Created basic command to reset a code Created basic command to check number of unallocated codes

---
+++ app/Console/Commands/DoorCode/Reset.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\DoorCode;
+
+use Illuminate\Console\Command;
+
+class Reset extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'doorcode:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
