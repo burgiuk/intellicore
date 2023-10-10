8585d9de1ac69296d672212085b531f09fb53165
John Burgess <j_burgess12@yahoo.com>
2023-10-10 17:24:28 +0100

    Created method to assign a code to someone - Added method docblocks - Added type checks to the parameters - Created method to check for number sequences greater than 3

--- app/Http/Controllers/DoorCodesController.php
+++ app/Http/Controllers/DoorCodesController.php
@@ -8,7 +8,12 @@ use Nette\Utils\Random;
 
 class DoorCodesController extends Controller
 {
-    public static function createDoorCodeFromName($name,int $length = 6){
+    /**
+     * @param string $name
+     * @param int $length
+     * @return void
+     */
+    public static function createDoorCodeFromName(string $name,int $length = 6){
         $code = Random::generate($length,'0-9');
 
         $doorcode = new DoorCodes();
@@ -24,6 +29,19 @@ class DoorCodesController extends Controller
         dd($doorcode);
     }
 
+    /**
+     * @param string $name
+     * @param string $code
+     * @return void
+     */
+    public static function assignDoorCode(string $name, string $code){
+        dd((new DoorCodesController)->checkSequenceLength($code));
+    }
+
+    /**
+     * @param $code
+     * @return bool
+     */
     private function checkRestrictions($code){
         // Code cannot be a palindrome
         if($code === strrev($code)){
@@ -53,6 +71,53 @@ class DoorCodesController extends Controller
             return false;
         }
 
-        // Character cannot be in a sequence of more than 3
+        return true;
+    }
+
+    /**
+     * This method checks the business rule:
+     * Character cannot be in a sequence of more than 3
+     * @param string $code
+     * @return bool
+     */
+    private function checkSequenceLength(string $code):bool{
+        // Sets up the previous character and sequence length
+        $prevChar = '';
+        $sequence = 0;
+
+        // Loops through the characters
+        foreach (str_split($code) as $character) {
+            // Checks if we are at the start of the door code
+            if(empty($prevChar)){
+                // Sets the current character to the previous character
+                $prevChar = $character;
+                // Sets the sequence length to 1 as we are at the first character
+                $sequence = 1;
+                continue;
+            }
+
+            // Calculates the next possible characters in sequence
+            $testUp = $prevChar + 1;
+            $testDown = $prevChar - 1;
+            // Sets the current character to the previous character
+            $prevChar = $character;
+
+            // Checks if the current character matches the possible characters
+            if($character == $testDown || $character == $testUp){
+                // Increments the sequence if above is true
+                $sequence++;
+                continue;
+            }
+            // Resets the sequence as nothing has matched
+            $sequence = 1;
+        }
+
+        // Checks if the sequence is greater than 3
+        if($sequence > 3){
+            return false;
+        }
+
+        // Door code is valid
+        return true;
     }
 }
